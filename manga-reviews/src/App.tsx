f3823f1805707af6b7c13e6fb8e17f73f6c4bcfc
DevByPblo <prodbypblo@gmail.com>
2025-07-24 00:14:30 +0100

    text

--- manga-reviews/src/App.tsx
+++ manga-reviews/src/App.tsx
@@ -1,15 +1,14 @@
-// src/App.tsx
-
+ 
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import AddMangaPage from './pages/AddMangaPage';
-import MangaDetailsPage from './pages/MangaDetailsPage';
-import SearchPage from './pages/Search';
-import NotFound from './pages/NotFound';
-import Genres from './pages/Genres';
+import Home from './pages/Home.tsx';
+import AddMangaPage from './pages/AddMangaPage.tsx';
+import MangaDetailsPage from './pages/MangaDetailsPage.tsx';
+import SearchPage from './pages/Search.tsx';
+import NotFound from './pages/NotFound.tsx';
+import Genres from './pages/Genres.tsx';
 import './App.css';
-import Navbar from './components/Navbar';
-import GenreMangaList from './pages/GenreMangaList';
+import Navbar from './components/Navbar.tsx';
+import GenreMangaList from './pages/GenreMangaList.tsx';
 
 
 export default function App() {
